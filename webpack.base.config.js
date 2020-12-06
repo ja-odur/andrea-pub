b7e656748bc25ab01b4cc2cb78295128f4d0128b
ja-odur <odurjoseph8@gmail.com>
2020-12-06 18:20:02 +0300

    configure webpack

---
+++ webpack.base.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const dotenv = require('dotenv');
+var webpack = require('webpack');
+
+function compact(items) {
+    return items.filter(item => item);
+}
+
+const config = () => {
+
+    const env = dotenv.config().parsed;
+
+    const envKeys = Object.keys(env).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(env[next]);
+        return prev;
+      }, {});
+
+    return {
+        context: __dirname,
+
+        entry: './frontend/index.tsx',
+
+        output: {
+            path: path.resolve('./frontend/static/bundles/'),
+            filename: '[name]-[hash].js'
+        },
+
+        plugins: [
+            new webpack.DefinePlugin(envKeys)
+        ],
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.sass$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.(png|je?pg|webp)$/,
+                    use: ['url-loader']
+                },
+                {
+                    test: /\.html$/,
+                    use: ['html-loader']
+                },
+                {
+                    enforce: "pre",
+                    test: /\.js$/,
+                    loader: "source-map-loader"
+                },
+                // Fonts
+                {
+                    test: /\.(woff2|woff|ttf|eot|svg)$/,
+                    // include: [/frontend\/fonts/],
+                    use: compact([
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[path][name].[ext]',
+                            },
+                        },
+                    ]),
+                },
+            ]
+        },
+
+        resolve: {
+            modules: ['node_modules'],
+            extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.scss', '.sass', '.png']
+        },
+
+    };
+};
+
+module.exports = config();
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+var config = require('./webpack.base.config.js');
+
+config.entry = './frontend/index.js'
+
+config.output.path = path.resolve('./frontend/static/webpack_bundles/')
+
+// Add BundleTracker plugin
+config.plugins =  config.plugins.concat([
+        new BundleTracker({filename: './webpack-stats.json'}),
+    ]);
+
+// Add a loader for JSX files
+config.module.rules.push(
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: ['babel-loader']
+  }
+);
+
+module.exports = config;

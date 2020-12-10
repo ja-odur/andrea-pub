f81513e6575b4251d816206b9d07276fd288e7b3
ja-odur <odurjoseph8@gmail.com>
2020-12-11 01:43:59 +0300

    add language example code for js and python

---
+++ frontend/components/editor/examples.js
@@ -0,0 +1,13 @@
+import rTabs from "utils/rTabs";
+const examples = {
+    19: rTabs(`
+    // Write JavaScript code in this online editor and run it.
+    console.log("Hello world");
+    `),
+    37: rTabs(`
+    # Write Python 3 code in this online editor and run it.
+    print("Hello world")
+    `),
+}
+
+export default examples;

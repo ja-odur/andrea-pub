8cd247e06493e310aeeb0c3116fbe9884a0071a9
ja-odur <odurjoseph8@gmail.com>
2020-12-19 23:54:56 +0300

    add fullscreen mode to editor

--- frontend/components/editor/editor.js
+++ frontend/components/editor/editor.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
 import { ControlledEditor } from '@monaco-editor/react';
-import config from "./editorConfigs";
-import examples from "./examples";
 import ReactSelect from "../selects/reactSelect";
 import { Resizable } from "re-resizable";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+
+import config from "./editorConfigs";
+import examples from "./examples";
+import FullScreenIcon from "../icons/fullscreen";
 
 import "styles/editor";
 
 const BAD_WORD = "eval";
 const WARNING_MESSAGE = " <- hey man, what's this?";
+
+const iconSize = '60%';
 
 export const HorizontalMultiResizable = ({leftComponent, rightComponent}) => {
     return (
@@ -39,22 +44,37 @@ export const HorizontalMultiResizable = ({leftComponent, rightComponent}) => {
 }
 
 export const OutputOrShell = () => {
+    const fullScreenHandle = useFullScreenHandle();
+    const [isFullScreen, setIsFullScreen] = useState(false)
     return (
-        <div className={"output-or-shell"}>
-            <div className={"editor-header no-border-left"}>
-                <span className={"text-editor-output-name"}>output</span>
-                <div className={"text-editor-command-wrapper"}>
-                    <button className={"editor-btn text-editor-btn-clear"}>clear</button>
+        <FullScreen
+            handle={fullScreenHandle}
+            onChange={(state) => setIsFullScreen(state)}
+        >
+            <div className={"output-or-shell"}>
+                <div className={"editor-header no-border-left"}>
+                    <span className={"text-editor-output-name"}>output</span>
+                    <div className={"text-editor-command-wrapper"}>
+                        <FullScreenIcon
+                            enterHandler={fullScreenHandle.enter}
+                            exitHandler={fullScreenHandle.exit}
+                            active={isFullScreen}
+                            height={iconSize}
+                        />
+                        <button className={"editor-btn text-editor-btn-clear"}>clear</button>
+                    </div>
+                </div>
+                <div className={"text-output-content"}>
+                    output text area
                 </div>
             </div>
-            <div className={"text-output-content"}>
-                output text area
-            </div>
-        </div>
+        </FullScreen>
     )
 }
 
 export const TextEditor = () => {
+    const fullScreenHandle = useFullScreenHandle();
+    const [isFullScreen, setIsFullScreen] = useState(false)
     const [language, setLanguage] = useState(config.availableLanguages[1]);
     const [theme, setTheme] = useState(config.defaultTheme);
 
@@ -71,36 +91,47 @@ export const TextEditor = () => {
     };
 
     return (
-        <div className={"text-editor"}>
-            <div className={"editor-header"}>
-                <span className={"text-editor-name"}>editor</span>
-                <div className={"text-editor-command-wrapper"}>
-                    <ReactSelect
-                        wrapperClassName={"text-editor-language-select"}
-                        defaultValue={language}
-                        options={config.availableLanguages}
-                        onChange={handleLanguageChange}
-                    />
-                    <button className={"editor-btn text-editor-btn-blue"}>run</button>
+        <FullScreen
+            handle={fullScreenHandle}
+            onChange={(state) => setIsFullScreen(state)}
+        >
+            <div className={"text-editor"}>
+                <div className={"editor-header"}>
+                    <span className={"text-editor-name"}>editor</span>
+                    <div className={"text-editor-command-wrapper"}>
+                        <FullScreenIcon
+                            enterHandler={fullScreenHandle.enter}
+                            exitHandler={fullScreenHandle.exit}
+                            active={isFullScreen}
+                            height={iconSize}
+                        />
+                        <ReactSelect
+                            wrapperClassName={"text-editor-language-select"}
+                            defaultValue={language}
+                            options={config.availableLanguages}
+                            onChange={handleLanguageChange}
+                        />
+                        <button className={"editor-btn text-editor-btn-blue"}>run</button>
+                    </div>
+                </div>
+                <div className={"text-editor-content"} id={"text-editor"}>
+                    <ControlledEditor
+                        height="100%"
+                        language={config.supportedLanguages[language.key].name}
+                        value={examples[config.supportedLanguages[language.key].exampleId]}
+                        onChange={handleEditorChange}
+                        theme={theme}
+                        options={{
+                            fontFamily: "Monaco, Menlo, 'Courier New', monospace",
+                            fontSize: "20px",
+                            codeLens: false,
+                            showUnused: true,
+                            showDeprecated: true,
+                        }}
+                      />
                 </div>
             </div>
-            <div className={"text-editor-content"} id={"text-editor"}>
-                <ControlledEditor
-                    height="100%"
-                    language={config.supportedLanguages[language.key].name}
-                    value={examples[config.supportedLanguages[language.key].exampleId]}
-                    onChange={handleEditorChange}
-                    theme={theme}
-                    options={{
-                        fontFamily: "Monaco, Menlo, 'Courier New', monospace",
-                        fontSize: "20px",
-                        codeLens: false,
-                        showUnused: true,
-                        showDeprecated: true,
-                    }}
-                  />
-            </div>
-        </div>
+        </FullScreen>
     )
 }
 

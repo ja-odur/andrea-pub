2b24588b66649fb0917ca0b9fa4fcd5a7b016f06
ja-odur <odurjoseph8@gmail.com>
2021-01-09 23:37:09 +0300

    update compiler endpoint to run input code

--- compiler/views.py
+++ compiler/views.py
@@ -2,7 +2,8 @@ from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.decorators import api_view
-from .serializers import CodeSerializer
+from .compilers import SSHCompiler
+from .serializers import CodeSerializer, CodeRunOutputSerializer
 
 
 @api_view(['POST'])
@@ -10,5 +11,8 @@ def compile_code(request):
     data = JSONParser().parse(request)
     code_serializer = CodeSerializer(data=data)
     if code_serializer.is_valid():
-        return JsonResponse(code_serializer.data, status=status.HTTP_200_OK)
+        code = code_serializer.save()
+        compiler = SSHCompiler(language=code.language)
+        code_output = compiler.exec_as_heredoc(code.code)
+        return JsonResponse(CodeRunOutputSerializer(code_output).data, status=status.HTTP_200_OK)
     return JsonResponse(code_serializer.errors, status=status.HTTP_400_BAD_REQUEST)

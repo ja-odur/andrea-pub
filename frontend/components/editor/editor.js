ba275f6660949f8f60ed5a3a420dc2b10df580aa
ja-odur <odurjoseph8@gmail.com>
2021-03-07 22:01:41 +0300

    add vertical-resizable on fullscreen

--- frontend/components/editor/editor.js
+++ frontend/components/editor/editor.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { ControlledEditor } from '@monaco-editor/react';
 import ReactSelect from "../selects/reactSelect";
-import { Resizable } from "re-resizable";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 import config from "./editorConfigs";
@@ -9,41 +8,15 @@ import examples from "./examples";
 import FullScreenIcon from "../icons/fullscreen";
 import { runCodeApi } from "apis/compiler";
 import DotLoader from "components/loaders/dotLoader";
+import { HorizontalMultiResizable, VerticalMultiResizable } from "components/resizable/multipleResizable";
 
 import "styles/editor";
 
 
 const iconSize = '60%';
 
-export const HorizontalMultiResizable = ({leftComponent, rightComponent}) => {
-    return (
-        <div
-            style={{
-                width: '100%',
-                display: 'flex',
-                overflow: 'hidden',
-            }}
-        >
-            <Resizable
-                // style={style}
-                defaultSize={{
-                  width: '50%',
-                  // height: 200,
-                }}
-                maxWidth="75%"
-                minWidth="50%"
-            >
-                {leftComponent}
-            </Resizable>
-            <div style={{ width: '100%', minWidth: '25%' }}>
-                {rightComponent}
-            </div>
 
-        </div>
-    )
-}
-
-export const OutputOrShell = ({ loading, codeOutput }) => {
+export const OutputOrShell = ({ loading, codeOutput, fullscreen }) => {
     const fullScreenHandle = useFullScreenHandle();
     const [isFullScreen, setIsFullScreen] = useState(false)
     return (
@@ -55,12 +28,15 @@ export const OutputOrShell = ({ loading, codeOutput }) => {
                 <div className={"editor-header no-border-left"}>
                     <span className={"text-editor-output-name"}>output</span>
                     <div className={"text-editor-command-wrapper"}>
-                        <FullScreenIcon
-                            enterHandler={fullScreenHandle.enter}
-                            exitHandler={fullScreenHandle.exit}
-                            active={isFullScreen}
-                            height={iconSize}
-                        />
+                        { fullscreen &&
+                              <FullScreenIcon
+                                  enterHandler={fullScreenHandle.enter}
+                                  exitHandler={fullScreenHandle.exit}
+                                  active={isFullScreen}
+                                  height={iconSize}
+                              />
+                        }
+
                         <button className={"editor-btn text-editor-btn-clear"}>clear</button>
                     </div>
                 </div>
@@ -98,6 +74,7 @@ export const TextEditor = ({ codeRunning, setCodeRunning, updateCodeOutput }) =>
     const [theme, setTheme] = useState(config.defaultTheme);
     const [code, setCode] = useState(extractCodeExample(language));
     const [isEditorClean, setIsEditorClean] = useState(true);
+    const [internalCodeOutput, setInternalCodeOutput] = useState({ output: [], error: [] })
 
     const handleEditorChange = (ev, code) => {
         setCode(code)
@@ -111,11 +88,16 @@ export const TextEditor = ({ codeRunning, setCodeRunning, updateCodeOutput }) =>
         }
     };
 
+    const setCodeOutput = (data) => {
+        setInternalCodeOutput(data)
+        updateCodeOutput(data)
+    }
+
     const handleRunCLick = async () => {
         setCodeRunning(true);
         await runCodeApi({ language: language.key, code })
             .then(data => {
-                updateCodeOutput(data)
+                setCodeOutput(data)
             })
             .catch(err => {
                 console.log(err.response.data)
@@ -123,66 +105,97 @@ export const TextEditor = ({ codeRunning, setCodeRunning, updateCodeOutput }) =>
         setCodeRunning(false);
     }
 
+    const editorComponent = (
+        <div className={"text-editor"}>
+            <div className={"editor-header"}>
+                <span className={"text-editor-name"}>editor</span>
+                <div className={"text-editor-command-wrapper"}>
+                    <FullScreenIcon
+                        enterHandler={fullScreenHandle.enter}
+                        exitHandler={fullScreenHandle.exit}
+                        active={isFullScreen}
+                        height={iconSize}
+                    />
+                    <ReactSelect
+                        wrapperClassName={"text-editor-language-select"}
+                        defaultValue={language}
+                        options={config.availableLanguages}
+                        onChange={handleLanguageChange}
+                    />
+                    <button
+                        onClick={handleRunCLick}
+                        disabled={codeRunning}
+                        className={`editor-btn ${codeRunning ? 'btn-disabled' : 'text-editor-btn-blue'}`}
+                    >
+                        run
+                    </button>
+                </div>
+            </div>
+            <div className={"text-editor-content"} id={"text-editor"}>
+                <ControlledEditor
+                    height="100%"
+                    language={config.supportedLanguages[language.key].name}
+                    value={code}
+                    onChange={handleEditorChange}
+                    theme={theme}
+                    options={{
+                        fontFamily: "Monaco, Menlo, 'Courier New', monospace",
+                        fontSize: "20px",
+                        codeLens: false,
+                        showUnused: true,
+                        showDeprecated: true,
+                    }}
+                  />
+            </div>
+        </div>
+    )
+
     return (
         <FullScreen
             handle={fullScreenHandle}
             onChange={(state) => setIsFullScreen(state)}
         >
-            <div className={"text-editor"}>
-                <div className={"editor-header"}>
-                    <span className={"text-editor-name"}>editor</span>
-                    <div className={"text-editor-command-wrapper"}>
-                        <FullScreenIcon
-                            enterHandler={fullScreenHandle.enter}
-                            exitHandler={fullScreenHandle.exit}
-                            active={isFullScreen}
-                            height={iconSize}
+            {isFullScreen ?
+                <VerticalMultiResizable
+                    topComponent={editorComponent}
+                    bottomComponent={
+                        <OutputOrShell
+                            loading={codeRunning}
+                            codeOutput={internalCodeOutput}
                         />
-                        <ReactSelect
-                            wrapperClassName={"text-editor-language-select"}
-                            defaultValue={language}
-                            options={config.availableLanguages}
-                            onChange={handleLanguageChange}
-                        />
-                        <button
-                            onClick={handleRunCLick}
-                            disabled={codeRunning}
-                            className={`editor-btn ${codeRunning ? 'btn-disabled' : 'text-editor-btn-blue'}`}
-                        >
-                            run
-                        </button>
-                    </div>
-                </div>
-                <div className={"text-editor-content"} id={"text-editor"}>
-                    <ControlledEditor
-                        height="100%"
-                        language={config.supportedLanguages[language.key].name}
-                        value={code}
-                        onChange={handleEditorChange}
-                        theme={theme}
-                        options={{
-                            fontFamily: "Monaco, Menlo, 'Courier New', monospace",
-                            fontSize: "20px",
-                            codeLens: false,
-                            showUnused: true,
-                            showDeprecated: true,
-                        }}
-                      />
-                </div>
-            </div>
+                    }
+                />
+                :
+                editorComponent
+
+            }
         </FullScreen>
     )
 }
 
 export default function Editor () {
     const [codeOutput, setCodeOutput] = useState({ output: [], error: [] })
-    const [loading, setLoading] = useState(false);
     const [codeRunning, setCodeRunning] = useState(false);
     return (
         <div className={"editor-container"}>
             <HorizontalMultiResizable
-                leftComponent={<TextEditor  codeRunning={codeRunning} setCodeRunning={setCodeRunning} updateCodeOutput={setCodeOutput} />}
-                rightComponent={<OutputOrShell loading={codeRunning} codeOutput={codeOutput} />}
+                leftComponent={
+                    <TextEditor
+                        codeRunning={codeRunning}
+                        setCodeRunning={setCodeRunning}
+                        updateCodeOutput={setCodeOutput}
+                    />}
+                rightComponent={
+                    <OutputOrShell
+                        loading={codeRunning}
+                        codeOutput={codeOutput}
+                        fullscreen
+                    />}
+                options={{
+                    defaultSize: "70%",
+                    maxWidth: "90%",
+                    minWidth: "50%",
+                }}
             />
         </div>
     )
